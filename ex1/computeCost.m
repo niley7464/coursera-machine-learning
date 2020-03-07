0f26e61f53eb9e2c0047c2cd6acb41ee06967e82
Yelin Jeong <yelin@Yelinui-MacBookPro.local>
2020-03-07 20:05:50 +0900

    ex1

---
+++ ex1/computeCost.m
@@ -0,0 +1,10 @@
+function J = computeCost(X, y, theta)
+
+m = length(y); % number of training examples
+
+temp = X * theta - y;
+temp = temp.^2;
+S = sum(temp(:));
+J = S/(2*m);
+
+end
---
+++ ex1/gradientDescent.m
@@ -0,0 +1,21 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+
+m = length(y); % number of training examples
+n = length(theta);
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    new_theta = theta;
+    for j = 1:n
+        temp = 0;
+        for i = 1:m
+            temp = temp + (X(i,:)*theta- y(i,:))*X(i,j);
+        end
+        new_theta(j,:) = new_theta(j,:) - alpha * temp / m;
+    end
+    theta = new_theta;
+
+    J_history(iter) = computeCost(X, y, theta);
+end
+end
